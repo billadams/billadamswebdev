ac3e99e802b2ba5b95e5ca074fe4da542180691c
Bill Adams <billadams1977@gmail.com>
2023-10-27 05:55:11 -0500

    updates

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,23 +1,65 @@
 'use Client';
 
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
+import styles from '@/components/modal/styles.module.css';
+import formStyles from '@/components/forms/styles.module.css';
 
 export default function Modal({ showModal }: { showModal: boolean }) {
+  const [showDialog, setShowDialog] = useState(showModal);
+
   const dialogRef = useRef<HTMLDialogElement>(null);
+
+  function getClasses(): string {
+    let className = '';
+
+    if (showDialog) {
+      className = styles.showDialog;
+    }
+
+    return className;
+  }
 
   if (showModal) {
     return (
-      <dialog id='contact-dialog' ref={dialogRef}>
-        <form method='dialog'>
-          <button type='button' id='close' aria-label='close' formNoValidate>
+      <dialog
+        id='contact-dialog'
+        className={`${styles.contactDialog} ${getClasses()}`}
+        ref={dialogRef}
+      >
+        <form method='dialog' className={styles.contactForm}>
+          <button
+            type='button'
+            id='close'
+            className={styles.closeButton}
+            aria-label='close'
+            formNoValidate
+          >
             X
           </button>
-          <label htmlFor='name'>Name:</label>
-          <input type='text' id='name'></input>
-          <label htmlFor='email'>Email:</label>
-          <input type='text' id='email'></input>
-          <label htmlFor='message'>Message:</label>
-          <textarea></textarea>
+          <div className={formStyles.formGroup}>
+            <label htmlFor='name'>Name:</label>
+            <input
+              type='text'
+              id='name'
+              className={formStyles.formControl}
+            ></input>
+          </div>
+          <div className={formStyles.formGroup}>
+            <label htmlFor='email'>Email:</label>
+            <input
+              type='text'
+              id='email'
+              className={formStyles.formControl}
+            ></input>
+          </div>
+          <div className={formStyles.formGroup}>
+            <label htmlFor='message'>Message:</label>
+            <textarea
+              id='message'
+              className={formStyles.formControl}
+            ></textarea>
+          </div>
+          <button type='submit'>Send It</button>
         </form>
       </dialog>
     );

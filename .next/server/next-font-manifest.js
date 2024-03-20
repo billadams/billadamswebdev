dfe2099668edf4454eec5e8631522d7138d5043e
Bill Adams <billadams1977@gmail.com>
2024-03-20 05:02:32 -0500

    Update to separate backend and frontend

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/billadams/Code/react/nextjs/billadamswebdev/src/app/layout\":[\"static/media/83d7d13e2307bc53-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"

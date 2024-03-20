641d77c9d84634b3ab9ca0ea28d969caf44ac5c3
Bill Adams <billadams1977@gmail.com>
2024-03-20 05:34:19 -0500

    Temp update to remove contact form.

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,23 +6,23 @@ import Contact from '@/components/contact/Contact';
 import styles from '@/components/header/header.module.css';
 
 export default function Header() {
-    const [showContactForm, setShowContactForm] = useState<boolean>(false);
+  const [showContactForm, setShowContactForm] = useState<boolean>(false);
 
-    const displayContactModal = () => {
-        setShowContactForm(true);
-    };
+  const displayContactModal = () => {
+    setShowContactForm(true);
+  };
 
-    return (
-        <header>
-            <Link href='/'>BA Web Dev</Link>
-            <button
+  return (
+    <header>
+      <Link href='/'>BA Web Dev</Link>
+      {/* <button
                 type='button'
                 className={`${styles.contactButton} primary-button`}
                 onClick={() => displayContactModal()}
             >
                 Contact Me
-            </button>
-            <Contact setShowForm={setShowContactForm} showForm={showContactForm} />
-        </header>
-    );
+            </button> */}
+      <Contact setShowForm={setShowContactForm} showForm={showContactForm} />
+    </header>
+  );
 }

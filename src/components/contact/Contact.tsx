738ccd9cc89ad3c7ba51ebaedaec2919013a4b0c
Bill Adams <billadams1977@gmail.com>
2024-03-22 10:09:21 -0500

    Update request body

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -181,10 +181,7 @@ export default function Contact({
     try {
       const response = await fetch('https://www.billadamswebdev.com/contact', {
         method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
+        body: formData,
       });
 
       const result = await response.json();

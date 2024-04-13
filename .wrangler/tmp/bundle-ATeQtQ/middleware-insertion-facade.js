bf0171c10f018bbbf4ab096d735043dfa1586fa3
Bill Adams <billadams1977@gmail.com>
2024-04-13 07:20:28 -0500

    commit .wrangler/

---
+++ .wrangler/tmp/bundle-ATeQtQ/middleware-insertion-facade.js
@@ -0,0 +1,27 @@
+				import worker, * as OTHER_EXPORTS from "/Users/billadams/Code/react/nextjs/billadamswebdev/.wrangler/tmp/pages-3SkhWF/functionsWorker-0.4214596836191442.mjs";
+				import * as __MIDDLEWARE_0__ from "/Users/billadams/.npm/_npx/32026684e21afda6/node_modules/wrangler/templates/middleware/middleware-ensure-req-body-drained.ts";
+import * as __MIDDLEWARE_1__ from "/Users/billadams/.npm/_npx/32026684e21afda6/node_modules/wrangler/templates/middleware/middleware-miniflare3-json-error.ts";
+				const envWrappers = [__MIDDLEWARE_0__.wrap,__MIDDLEWARE_1__.wrap].filter(Boolean);
+				const facade = {
+					...worker,
+					envWrappers,
+					middleware: [
+						__MIDDLEWARE_0__.default,__MIDDLEWARE_1__.default,
+            ...(worker.middleware ? worker.middleware : []),
+					].filter(Boolean)
+				}
+				export * from "/Users/billadams/Code/react/nextjs/billadamswebdev/.wrangler/tmp/pages-3SkhWF/functionsWorker-0.4214596836191442.mjs";
+
+				const maskDurableObjectDefinition = (cls) =>
+					class extends cls {
+						constructor(state, env) {
+							let wrappedEnv = env
+							for (const wrapFn of envWrappers) {
+								wrappedEnv = wrapFn(wrappedEnv)
+							}
+							super(state, wrappedEnv);
+						}
+					};
+				
+
+				export default facade;

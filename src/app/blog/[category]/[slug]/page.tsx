cfa17d5c2c5dc6d795c273cb42cf4422f3888b87
Bill Adams <billadams1977@gmail.com>
2024-08-11 10:23:25 -0500

    Blog updates

--- src/app/blog/[category]/[slug]/page.tsx
+++ src/app/blog/[category]/[slug]/page.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import { Metadata } from 'next';
 
+export const runtime = 'experimental-edge';
+
 export const metadata: Metadata = {
   title: 'Bill Adams Web Dev - Blog',
 };
 
-export default function Page() {
+export default function Page({
+  params,
+}: {
+  params: { category: string; slug: string };
+}) {
   return (
     <main>
-      <h1>Blog</h1>
+      <hgroup>
+        <h1>Using Moment.js</h1>
+        <p>Category: {params.category} | Published on: Jan 1st, 2024</p>
+      </hgroup>
+
       <p>This is the blog page.</p>
     </main>
   );
